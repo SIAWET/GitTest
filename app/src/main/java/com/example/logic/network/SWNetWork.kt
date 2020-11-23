1f5fbaf6fb1620307d24376ddbab5c58956f8065
SiaWet <3058454461@qq.com>
2020-11-23 16:16:19 +0800

    2020.11.14

--- app/src/main/java/com/example/logic/network/SWNetWork.kt
+++ app/src/main/java/com/example/logic/network/SWNetWork.kt
@@ -8,8 +8,16 @@ import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 object SWNetWork {
+    private val weatherService = ServiceCreater.create(WeatherService::class.java)
+    suspend fun getDW(lng: String, lat: String) =
+        weatherService.getDailyWeather(lng, lat).await()
+
+    suspend fun getRW(lng: String, lat: String) =
+        weatherService.getRealtimeWeather(lng, lat).await()
+
     private val Service = ServiceCreater.create(PlaceService::class.java)
     suspend fun searchP(query: String) = Service.searchPlaces(query).await()
+
     private suspend fun <T> Call<T>.await(): T {
         return suspendCoroutine { continuation ->
             enqueue(object : Callback<T> {
